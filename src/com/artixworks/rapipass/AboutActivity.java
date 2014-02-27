94c6e9fb959f15996e01db97f7c190135cd996c0
Rosendo Ropher Hernández <ropherpanama@gmail.com>
2014-02-27 22:33:17 +0000

    Se agregan logos para la pantalla about us

--- src/com/artixworks/rapipass/AboutActivity.java
+++ src/com/artixworks/rapipass/AboutActivity.java
@@ -1,42 +1,49 @@
 package com.artixworks.rapipass;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
-import android.os.Bundle;
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import com.artix.rapipass.commons.Utils;
 
+import com.artix.rapipass.commons.Utils;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_about)
 public class AboutActivity extends Activity {
 
 	private Typeface font;
+	@ViewById TextView textViewSoftName;
+	@ViewById TextView textViewSoftVersion;
+	@ViewById TextView textViewDerechos;
+	@ViewById ImageView icon_facebook;
+	@ViewById ImageView icon_twitter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about);
+	@AfterViews
+	void initializeView() {
 		//getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		font = Utils.loadFont(getApplicationContext());
-		TextView textName = (TextView) findViewById(R.id.textViewSoftName);
-		TextView textVersion = (TextView) findViewById(R.id.textViewSoftVersion);
-		TextView textDerechos = (TextView) findViewById(R.id.textViewDerechos);
-		textName.setTypeface(font);
-		textName.setTextSize(26);
-		textVersion.setTypeface(font);
-		textVersion.setTextSize(14);
-		textDerechos.setTypeface(font);
-		textDerechos.setTextSize(15);
+		textViewSoftName.setTypeface(font);
+		textViewSoftName.setTextSize(26);
+		textViewSoftVersion.setTypeface(font);
+		textViewSoftVersion.setTextSize(14);
+		textViewDerechos.setTypeface(font);
+		textViewDerechos.setTextSize(15);
 	}
-
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.about, menu);
-//		return true;
-//	}
 	
-	@Override
-	public void onBackPressed() {
-		super.onBackPressed();
-		finish();
+	public void openFacebook(View v) {
+		Uri uri = Uri.parse("http://www.facebook.com");
+		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(intent);
+	}
+	
+	public void openTwitter(View v) {
+		Uri uri = Uri.parse("http://www.twitter.com");
+		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(intent);
 	}
 }
